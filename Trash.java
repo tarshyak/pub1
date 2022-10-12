9151c916ae3903825b007886dd215df14134610d
tarshyak <115622018+tarshyak@users.noreply.github.com>
2022-10-12 17:36:50 +0530

    Update Trash.java

--- Trash.java
+++ Trash.java
@@ -2,7 +2,7 @@ public class Trash{
  
   public static void main(String[] args){
     
-   System.out.println("Hi Tarshya!!"+\n+"Welcome to Jenkins"); 
+   System.out.println("Hi Tarshya!! \n Welcome to Jenkins"); 
   }
   
 }
